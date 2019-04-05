e3ecd9bbc6ca4935819cb31e5b6c8fbf4f1a39c4
Lynn Kirby <lynn@arrak.is>
2019-04-05 15:44:00 -0400

    Rasterize logotype from text instead of outline

--- assets/logotype/README.md
+++ assets/logotype/README.md
@@ -1,7 +1,5 @@
 # Arrak.is Logotype
 
-![logotype.svg](logotype.svg)
-
 Typeset in Futura BT Heavy. I prefer using free software fonts but only Futura
 had the look I was going for. Because it's proprietary (and not used anywhere
 else on my site) I've only uploaded the SVG.
--- assets/logotype/rasterize-logotype.js
+++ assets/logotype/rasterize-logotype.js
@@ -4,39 +4,39 @@
 const fs = require("fs-extra");
 const path = require("path");
 const process = require("process");
-const sharp = require("sharp");
+const execa = require("execa");
 
-async function main(argv) {
-  if (argv.length !== 5) {
+async function rasterize(width, color, outputFile) {
+  const svg = await fs.readFile(path.join(__dirname, "logotype.svg"), { encoding: "utf8" });
+
+  const updatedSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <style>
+        #logotype {
+          fill: ${color};
+        }
+      </style>
+      ${svg}
+    </svg>
+  `;
+
+  await execa("inkscape", [
+    "--file", "-",
+    "--export-png", outputFile,
+    "--export-width", width,
+  ], { input: updatedSVG });
+
+  console.log(`Wrote image to ${outputFile}`);
+}
+
+async function main() {
+  if (process.argv.length !== 5) {
     console.log(`Usage: ${name} <pixel width> <CSS color> <output file>`);
     return;
   }
 
-  const width = Number.parseInt(argv[2]);
-  const color = argv[3];
-  const output = argv[4];
-  const svg = await fs.readFile(path.join(__dirname, "logotype.svg"), { encoding: "utf8" });
-
-  // Since Sharp just rasterizes the SVG immediately and doesn't perform any
-  // SVG-aware scaling, we have to do it ourselves.
-  // TODO: Replace this with a wrapper SVG that embeds the file instead of
-  // using magic strings.
-  const baseWidth = 310;
-  const baseHeight = 56;
-  const scale = width <= baseWidth ? 1 : Math.ceil((width / baseWidth) * 1.1);
-  const newWidth = baseWidth * scale;
-  const newHeight = baseHeight * scale;
-  const updatedSVG = svg
-    .replace('<g fill="#000000">', `<g fill="${color}" transform="scale(${scale})">`)
-    .replace('viewBox="0 0 310 56"', `viewBox="0 0 ${newWidth} ${newHeight}"`);
-
-
-  await sharp(Buffer.from(updatedSVG, "utf8"))
-    .resize(width)
-    .png()
-    .toFile(output);
-
-  console.log(`Wrote image to ${output}`);
+  const [node, script, width, color, outputFile] = process.argv;
+  await rasterize(Number.parseInt(width), color, outputFile);
 }
 
-main(process.argv).catch(error => console.error(error));
+main().catch(error => console.error(error));
