b8bcf30ed2c4c4ef4222ffee0901ed740a15e8c7
Lynn Kirby <lynn@arrak.is>
2019-06-07 05:27:38 -0400

    Generate _headers file with preload rules

--- src/_redirects.11ty.js
+++ src/_redirects.11ty.js
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: CC0-1.0
+// Written 2019 Lynn Kirby
+//
+// Generate Netlify _redirects file based on redirects specified in frontmatter.
+
 module.exports = class {
   data() {
     return {
@@ -8,16 +13,26 @@ module.exports = class {
 
   render({ collections }) {
     return collections.all.flatMap(page => {
-      const from = page.data.redirect;
+      let redirects = page.data.redirect;
       const to = page.url;
 
-      if (typeof from !== "string") return [];
+      if (redirects === null || redirects === undefined) return [];
 
-      if (!from.startsWith("/")) {
-        throw new Error(`${page.inputPath}: only absolute URL redirects implemented`);
+      if (typeof redirects === "string") {
+        redirects = [redirects];
       }
 
-      return `${from} ${to}`;
+      if (!Array.isArray(redirects)) {
+        throw new Error(`${page.inputPath}: expected "redirect" to be a string or array`);
+      }
+
+      return redirects.flatMap(from => {
+        if (!from.startsWith("/")) {
+          throw new Error(`${page.inputPath}: only absolute URL redirects implemented`);
+        }
+
+        return `${from} ${to}`;
+      });
     }).join("\n");
   }
 };
